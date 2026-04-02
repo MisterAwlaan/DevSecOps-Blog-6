afd01166a4fb5eb307cc9d3ef7b3c649cafc94d9
4lizee <alizeebochaton17@gmail.com>
2026-04-02 14:54:29 +0200

    Fix CSRF + maj requirements

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
+from flask_wtf.csrf import CSRFProtect
 import os
 
 app = Flask(__name__)
 app.secret_key = 'cle_secrete_super_securisee_pour_le_dev'
+csrf = CSRFProtect(app)
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
